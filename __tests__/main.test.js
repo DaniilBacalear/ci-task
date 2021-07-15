96948b9954c6693e6edd40b6556c8e40411c1c4c
Daniil Bacalear <dbacale@LLC02NP6G4G3QC.ngco.com>
2021-07-15 12:52:49 -0400

    added failed test

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -2,5 +2,8 @@
 describe("theses are my tests", () => {
     test('adds 1 + 2 to equal 3', () => {
         expect(1+2).toBe(3);
-      });
+    });
+    test('adds 1 + 2 to equal 3', () => {
+        expect(1+3).toBe(3);
+    });
  })
